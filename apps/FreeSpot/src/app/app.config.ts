39d7da69bf3ac4caf6e4edb3dc3173ac8989f4da
ComsaEnoh <comsaenoh22@gmail.com>
2024-06-14 19:40:54 +0300

    expermiental zoneless config

--- apps/FreeSpot/src/app/app.config.ts
+++ apps/FreeSpot/src/app/app.config.ts
@@ -1,11 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
     provideRouter(appRoutes), provideAnimationsAsync(),
   ],
 };
