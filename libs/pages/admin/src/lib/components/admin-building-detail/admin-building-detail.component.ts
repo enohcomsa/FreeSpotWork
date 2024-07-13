019d4d32d35074f26663bd53c1261b3cf9e9c9bf
ComsaEnoh <comsaenoh22@gmail.com>
2024-07-14 01:07:21 +0300

    building detail cleanup

--- libs/pages/admin/src/lib/components/admin-building-detail/admin-building-detail.component.ts
+++ libs/pages/admin/src/lib/components/admin-building-detail/admin-building-detail.component.ts
@@ -52,31 +52,6 @@ export class AdminBuildingDetailComponent implements OnInit {
   editingFloor = false;
   addFloorFormControl = this._formBuilder.nonNullable.control('');
 
-  floorExp: Floor = {
-    name: 'UTCN Obs ground Floor',
-    buildingName: 'Laboratoare Observator',
-    roomList: [],
-    totalSpotsNumber: 120,
-    freeSpots: 90,
-    busySpots: 20,
-    unavailableSpots: 10,
-  };
-  floorExp2: Floor = {
-    name: '328',
-    buildingName: 'Laboratoare Observator',
-    roomList: [],
-    totalSpotsNumber: 90,
-    freeSpots: 50,
-    busySpots: 30,
-    unavailableSpots: 10,
-  };
-  buildig: Building = {
-    name: 'Laboratoare Observator',
-    adress: 'Observatorului, 400347',
-    floorList: [this.floorExp, this.floorExp2],
-    specialEvent: false,
-  };
-
   ngOnInit(): void {
     this._adminFloorService.init();
     this._adminBuildingService.init();
