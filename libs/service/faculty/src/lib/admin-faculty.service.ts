a4d732e21b3f352847fa63f17a6897ec12cd01e9
ComsaEnoh <comsaenoh22@gmail.com>
2024-07-15 15:18:08 +0300

    faculty service lib added

---
+++ libs/service/faculty/src/lib/admin-faculty.service.ts
@@ -0,0 +1,43 @@
+import { computed, inject, Injectable, signal, Signal, WritableSignal } from '@angular/core';
+import { Faculty } from '@free-spot/models';
+import { SignalArrayUtil } from '@free-spot/util';
+import { HttpFacultyService } from '@http-free-spot/faculty';
+import { take } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminFacultyService {
+  private _httpFacultyService: HttpFacultyService = inject(HttpFacultyService);
+
+  private _facultyListSig: WritableSignal<Faculty[]> = signal([]);
+  facultyListSig = this._facultyListSig.asReadonly();
+
+  init(): void {
+    this._httpFacultyService
+      .getFacultyList()
+      .pipe(take(1))
+      .subscribe((facultyList: Faculty[]) => {
+        this._facultyListSig.set(facultyList.filter((faculty: Faculty) => faculty !== null));
+      });
+  }
+
+  getFacultyByName(facultyName: string): Signal<Faculty> {
+    return computed(() => this.facultyListSig().find((faculty: Faculty) => faculty.name === facultyName) || ({} as Faculty));
+  }
+
+  addFaculty(newFaculty: Faculty): void {
+    SignalArrayUtil.addItem(newFaculty, this._facultyListSig);
+    this._httpFacultyService.storeFacultyList(this._facultyListSig());
+  }
+
+  updateFaculty(oldFaculty: Faculty, updatedFaculty: Faculty): void {
+    SignalArrayUtil.replaceItem(oldFaculty, this._facultyListSig, updatedFaculty);
+    this._httpFacultyService.storeFacultyList(this._facultyListSig());
+  }
+
+  deleteFaculty(deletedFaculty: Faculty): void {
+    SignalArrayUtil.deleteItem(deletedFaculty, this._facultyListSig);
+    this._httpFacultyService.storeFacultyList(this._facultyListSig());
+  }
+}
