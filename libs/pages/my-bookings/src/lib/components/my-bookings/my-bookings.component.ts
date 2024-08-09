0bbe953c058f5ae83050d07016f4bcc85c85725d
ComsaEnoh <comsaenoh22@gmail.com>
2024-08-09 22:01:06 +0300

    my booking sort and filter added

--- libs/pages/my-bookings/src/lib/components/my-bookings/my-bookings.component.ts
+++ libs/pages/my-bookings/src/lib/components/my-bookings/my-bookings.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, computed, inject, OnInit, Signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, OnInit, signal, Signal, WritableSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatChipsModule } from '@angular/material/chips';
 import { DynamicFormComponent } from '@free-spot/ui';
@@ -6,7 +6,7 @@ import { BookedSpotComponent } from '../booked-spot/booked-spot.component';
 import { UserService } from '@free-spot-service/user';
 import { BookedEvent, FreeSpotDate, FreeSpotUser } from '@free-spot/models';
 import { AppDateService } from '@free-spot-service/app-date';
-import { WeekParity } from '@free-spot/enums';
+import { Event, WeekParity } from '@free-spot/enums';
 
 @Component({
   selector: 'free-spot-my-bookings',
@@ -37,21 +37,57 @@ export class MyBookingsComponent implements OnInit {
       return WeekParity.ODD;
     }
   });
-
-  filteredBookedItems: Signal<BookedEvent[]> = computed(() => {
+  private _activeBookedItemsSig: Signal<BookedEvent[]> = computed(() => {
     let bookedEventList: BookedEvent[] = this.currentUserSig().bookingList;
-    bookedEventList = bookedEventList?.filter((bookedEvent: BookedEvent) =>
-      bookedEvent.weekParity === this.weekParitySig() &&
-      new Date().setHours(0, 0, 0, 0) - new Date(bookedEvent.date).getTime() <= 0 &&
-      new Date().setHours(0, 0, 0, 0) - new Date(bookedEvent.date).getTime() === 0
-        ? new Date().getHours() < bookedEvent.startHour
-        : true,
-    );
+    bookedEventList = bookedEventList?.filter((bookedEvent: BookedEvent) => {
+      return (
+        (bookedEvent.weekParity === this.weekParitySig() || bookedEvent.weekParity === WeekParity.BOTH) &&
+        new Date().setHours(0, 0, 0, 0) - new Date(bookedEvent.date).getTime() <= 0 &&
+        (new Date().setHours(0, 0, 0, 0) - new Date(bookedEvent.date).getTime() === 0
+          ? new Date().getHours() < bookedEvent.startHour
+          : true)
+      );
+    });
+
+    bookedEventList = bookedEventList?.sort((event1, event2) => this._sortEventsByDate(event1, event2));
+
     return bookedEventList;
   });
+  private _eventFilter: WritableSignal<Event | null> = signal(null);
+
+  filteredBookedItemsSig: Signal<BookedEvent[]> = computed(() => {
+    let bookedEventList: BookedEvent[] = this._activeBookedItemsSig();
+
+    if (this._eventFilter() !== null) {
+      bookedEventList = bookedEventList = bookedEventList?.filter(
+        (bookedEvent: BookedEvent) => bookedEvent.activityType === this._eventFilter(),
+      );
+    }
+
+    return bookedEventList;
+  });
+  EVENT = Event;
+
+  filterActiveBookedItems(event: Event, all: boolean): void {
+    if (all) {
+      this._eventFilter.set(null);
+    } else {
+      this._eventFilter.set(event);
+    }
+  }
 
   ngOnInit(): void {
     this._userService.init();
     this._appDateService.init();
+  }
+
+  private _sortEventsByDate(event1: BookedEvent, event2: BookedEvent): number {
+    const date1: Date = new Date(event1.date);
+    const date2: Date = new Date(event2.date);
+    if (date1.getTime() === date2.getTime()) {
+      return event1.startHour - event2.startHour;
+    } else {
+      return date1.getTime() - date2.getTime();
+    }
   }
 }
